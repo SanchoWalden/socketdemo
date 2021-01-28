0c0294ee646ce256e98740443da2d951b3981edf
DiaLong <310571960@qq.com>
2021-01-28 16:28:44 +0800

    init

---
+++ SocketServer V4 dotnet 2.0/TestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Action action = new Action(StartClient);
+ 
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Task.Factory.StartNew(action);
+            Application.Run(new FormServer());
+        }
+
+        static void StartClient()
+        {
+            FormClient formClient = new FormClient();
+            formClient.ShowDialog();
+        }
+    }
+}
